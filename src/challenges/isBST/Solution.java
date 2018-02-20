8577f64f9554a2147deb86bdb2bb8bc262af5939
Piyush Nikam <pnikam@manh.com>
2018-02-20 18:43:38 +0530

    isBST completed

--- src/challenges/isBST/Solution.java
+++ src/challenges/isBST/Solution.java
@@ -11,37 +11,75 @@ The Node class is defined as follows:
 */
 
 class Node {
-    int data;
-    Node left;
-    Node right;
- }
+	int data;
+	Node left;
+	Node right;
+}
 
-public class Solution{
-	 boolean checkBST(Node root) {
-	        return 
-	        ( CheckSubTree(root.left, root.data, 0) &&
-	        CheckSubTree(root.right, root.data, 1) );
-	    }
+public class Solution {
 
-	    boolean CheckSubTree(Node node, int data, int direction){ //0: left, 1: right
-	        if(node == null)
-	            return true;
-	        
-	        if(direction == 1){
-	            if(node.data <= data)
-	                return false;
-	            else
-	            {
-	                return (CheckSubTree(node.left,node.data,0) && CheckSubTree(node.right,node.data,1));
-	            }
-	            
-	        }else{
-	            if(node.data >= data)
-	                return false;
-	            else{
-	                return (CheckSubTree(node.left,node.data,0) && CheckSubTree(node.right,node.data,1));
-	            }
-	        }
+	boolean checkBST(Node root) {
+		if (root == null)
+			return true;
 
-	    }
+		Boolean isBST = true;
+
+		if (root.left != null) {
+			isBST = checkLeftSubtree(root.left, null, root.data);
+		}
+
+		if (root.right != null) {
+			isBST &= checkRightSubtree(root.right, root.data, null);
+		}
+		return isBST;
+	}
+
+	boolean checkLeftSubtree(Node node, Integer leftLimit, Integer rightLimit) {
+		if (node == null)
+			return true;
+
+		if (rightLimit != null && node.data >= rightLimit) {
+			return false;
+		}
+
+		if (leftLimit != null && node.data <= leftLimit) {
+			return false;
+		}
+
+		boolean isBST = true;
+		if (node.left != null) {
+			isBST = checkLeftSubtree(node.left, leftLimit, node.data);
+		}
+
+		if (node.right != null) {
+			isBST &= checkRightSubtree(node.right, node.data, rightLimit);
+		}
+
+		return isBST;
+	}
+
+	boolean checkRightSubtree(Node node, Integer leftLimit, Integer rightLimit) {
+		if (node == null)
+			return true;
+
+		if (leftLimit != null && node.data <= leftLimit) {
+			return false;
+		}
+
+		if (rightLimit != null && node.data >= rightLimit) {
+			return false;
+		}
+
+		boolean isBST = true;
+		if (node.left != null) {
+			isBST = checkLeftSubtree(node.left, leftLimit, node.data);
+		}
+
+		if (node.right != null) {
+			isBST &= checkRightSubtree(node.right, node.data, rightLimit);
+		}
+
+		return isBST;
+	}
+
 }
